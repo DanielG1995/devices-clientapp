ace05de456240928180f3c557400548e0e47eaca
DanielG1995 <54053579+DanielG1995@users.noreply.github.com>
2021-12-11 02:58:44 -0500

    Update AddButton.tsx
    FIX: fix class name

--- src/Components/AddButton.tsx
+++ src/Components/AddButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const AddButton = ({ setOpenModal, openModal }: { setOpenModal: CallableFunction, openModal: boolean }) => {
     return (
-        <div className='btn__add d-flex justifi-content-center align-items-center'>
+        <div className='btn__add d-flex justify-content-center align-items-center'>
             <div onClick={() => { setOpenModal(!openModal) }} className='col-12 d-flex justify-content-center'>
                 <Add fontSize='large' titleAccess='Add Device'>Add Device</Add>
             </div>
